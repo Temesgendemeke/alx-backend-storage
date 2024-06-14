f258c3dfe53a237f36f1ca7935df861df3a9015b
Temesgen dora <tdemeke36@gmail.com>
2024-06-14 09:55:22 -0700

    fixed: task 3 #2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 --  lists all bands with Glam rock
-SELECT band_name, COALESCE(split, 2020) - COALESCE(formed, 0) AS lifespan  From holberton.metal_bands 
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan  From metal_bands 
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

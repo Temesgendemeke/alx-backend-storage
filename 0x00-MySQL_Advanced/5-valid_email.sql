8672f6f428a47b3915c47709f0de52405042977a
Temesgen dora <tdemeke36@gmail.com>
2024-06-14 11:59:15 -0700

    fix: task 3

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,5 @@
 -- valide resets attribute valide email
+DELIMITER $$
 CREATE TRIGGER reset_attribute BEFORE UPDATE
 ON user 
 FOR EACH ROW
@@ -6,4 +7,5 @@ BEGIN
     IF NEW.email != OLD.email 
     THEN SET NEW.valid_email = 0;
     END IF;
-END;
+END $$
+DELIMITER;

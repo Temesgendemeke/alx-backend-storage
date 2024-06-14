22e5d649bda47bb662db26e765755e27ef1a94fc
Temesgen dora <tdemeke36@gmail.com>
2024-06-14 10:39:14 -0700

    fixed: task 3 #2

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- trigger that decreases the quantity of an item
+-- after adding a new order.
+
+CREATE TRIGGER update_quantity_after_new_order
+AFTER INSERT ON order 
+FOR EACH ROW
+UPDATE items
+SET NEW.quantity = OLD.quantity - order.number

7b8249ba7f954c10aa40a2dbc94bb31c83f80306
Temesgen dora <tdemeke36@gmail.com>
2024-06-14 09:46:05 -0700

    done task 2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
+--  lists all bands with Glam rock
 SELECT band_name, COALESCE(split, 2022) - COALESCE(formed, 0) AS lifespan  From holberton.metal_bands 
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

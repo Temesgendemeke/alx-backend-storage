89deae497750f9187f971f008908076815835298
Temesgen dora <tdemeke36@gmail.com>
2024-06-15 13:16:43 -0700

    done: task 101

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,9 @@
+-- create procedure
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, 
+                          IN project_name VARCHAR(255)
+                          in score FLOAT)
+BEGIN
+    
+END $$
+DELIMITER ; 

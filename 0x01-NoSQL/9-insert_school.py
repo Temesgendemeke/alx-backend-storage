d8cd0673bed27def5f6d5e23942344473775b9f6
Temesgen dora <tdemeke36@gmail.com>
2024-06-17 09:47:04 -0700

    fixed task 10

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,4 +6,3 @@ def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
     newdoc = mongo_collection.insert_one(kwargs)
     return newdoc.inserted_id
-    

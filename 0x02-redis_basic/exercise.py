36282f0dc7427891331bad434c674f38b92a8047
Temesgen dora <tdemeke36@gmail.com>
2024-06-19 09:36:42 -0700

    fixed: task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,16 +2,17 @@
 """ Redis """
 import redis
 import uuid
+import typing
 
 
 class Cache:
     """_summary_"""
-    def __init__(self):
+    def __init__(self) -> None:
         """_summary_"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         """_summary_"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)

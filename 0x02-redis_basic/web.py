12dbd500e86f66692dce78be008427b795bc2bd4
Temesgen dora <tdemeke36@gmail.com>
2024-06-19 10:00:14 -0700

    task 1

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,32 @@
-import requests
+#!/usr/bin/env python3
+""" Redis Module """
+
+from functools import wraps
 import redis
-import time
+import requests
 from typing import Callable
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_ = redis.Redis()
 
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    cache_key = f"count:{url}"
-    
-
-    redis_client.incr(cache_key)
-    
-    cached_content = redis_client.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
-    
-    response = requests.get(url)
-    html_content = response.text
-    
-    redis_client.setex(url, 10, html_content)
-    
-    return html_content
-
-
-if __name__ == "__main__":
-    test_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
-    for _ in range(3):
-        start_time = time.time()
-        content = get_page(test_url)
-        end_time = time.time()
-        print(f"Time taken: {end_time - start_time:.2f} seconds")
-        print(f"Content length: {len(content)}")
-        print(f"Access count: {redis_client.get(f'count:{test_url}').decode('utf-8')}")
-        print("---")
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
